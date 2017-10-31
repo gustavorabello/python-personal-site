6d54801155b59c032b38a0c168828632c64e71a4
Gustavo ANJOS <gustavo.rabello@gmail.com>
2017-10-31 14:49:46 -0200

    New procedure to delete files and folders from deploy folder without deleting .gitignore.

--- build.py
+++ build.py
@@ -192,14 +192,13 @@ def genSite():
  if os.path.exists(directory):
   print ""
   print "Removing contents in " + directory
-  for the_file in os.listdir(directory):
-   file_path = os.path.join(directory, the_file)
-   try:
-    if os.path.isfile(file_path):
-     os.unlink(file_path)
-     #elif os.path.isdir(file_path): shutil.rmtree(file_path)
-   except Exception as e:
-    print(e)
+  for dirs in os.listdir(directory):
+   print dirs
+   if os.path.isdir(directory + '/' + dirs):
+    shutil.rmtree(directory + '/' + dirs)
+   else: # it is a file
+    if dirs != '.gitignore':
+     os.remove(directory + '/' + dirs)
  else:
   print ""
   print "Creating " + directory
